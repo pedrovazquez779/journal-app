f76cfab93d7c4ef53d6fb70eea1c67821aba41c5
Pedro Vazquez <psv779@gmail.com>
2024-05-22 10:38:55 -0300

    Removing secrets

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,20 +2,10 @@
 import {initializeApp} from 'firebase/app';
 import {getAuth} from 'firebase/auth';
 import {getFirestore} from 'firebase/firestore/lite';
+import {firebaseConfig} from './credentials';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// Make sure to complete this information with your own configuration
-const firebaseConfig = {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: ''
-};
 
 // Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig);
---
+++ src/firebase/credentials.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+// Make sure to complete this information with your own configuration
+export const firebaseConfig = {
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: ''
+};
